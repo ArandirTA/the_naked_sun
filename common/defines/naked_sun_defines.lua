72590fc9cd871c7f59369b4f30e4953a89d67541
ArandirTA <ghislain.petiet@polytechnique.org>
2016-06-10 02:37:17 +0200

    Test 2

--- common/defines/naked_sun_defines.lua
+++ common/defines/naked_sun_defines.lua
@@ -1,3 +1,3 @@
 NDefines.NGraphics.ORBIT_HSV = {0.8,0.8,0.8}
 NDefines.NGraphics.DEFAULT_PLANET_PLANE = -100
-NDefines.NGameplay.CORE_SECTOR_PLANET_CAP = 90
+NDefines.NGameplay.CORE_SECTOR_PLANET_CAP = 900
